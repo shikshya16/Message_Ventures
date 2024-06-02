88a1aa60109fd38ac2a9fc97fac9c189440b3294
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 20:37:51 +0545

    Add user model schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,29 +1,35 @@
 const mongoose = require ('mongoose');
 
 const userSchema = new mongoose.Schema ({
+
     fullName : {
         type : String ,
         required : true
     },
+    
     userName : {
         type : String ,
         required : true ,
         unique : true
     } ,
+
     password : {
         type : String ,
         required : true ,
         minlength : 8
     },
+
     gender : {
         type : String ,
         required : true ,
         enum : ["male" , "female"]
     } ,
+
     profilePic : {
         type : String ,
         default : ""
     } ,
+
 }, { timestamps: true}) ;
 
 const User = mongoose.model ("User" , userSchema) ;
