36b30381cd76e36b9f81a601fe147252ef4190e1
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 21:29:36 +0545

    Added SignUp component with form for user registration and integrated signup logic using useSignup hook.

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import useSignup from "../../hooks/useSignup";
 
 const SignUp = () => {
+	
 	const [inputs, setInputs] = useState({
 		fullName: "",
 		userName: "",
