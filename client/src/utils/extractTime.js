c805d7b8d388603d4ee9cb58f44135c491003332
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 21:32:41 +0545

    Implemented extractTime function to extract time from a given date string and a helper function padZero for formatting numbers with leading zeros

--- client/src/utils/extractTime.js
+++ client/src/utils/extractTime.js
@@ -1,7 +1,9 @@
 export function extractTime(dateString) {
+
 	const date = new Date(dateString);
 	const hours = padZero(date.getHours());
 	const minutes = padZero(date.getMinutes());
+	
 	return `${hours}:${minutes}`;
 }
 
