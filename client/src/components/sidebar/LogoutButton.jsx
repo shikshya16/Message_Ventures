d298e192ea6597e32530d38ff96035b77ba86091
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 21:07:40 +0545

    LogoutButton component to display logout button with logout functionality.

--- client/src/components/sidebar/LogoutButton.jsx
+++ client/src/components/sidebar/LogoutButton.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+
 import { BiLogOut } from "react-icons/bi";
+
 import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+	
 	const { loading, logout } = useLogout();
 
 	return (
