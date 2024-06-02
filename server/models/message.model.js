382c85f0f952d294a188f1cff8ae066e383108f9
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 20:37:09 +0545

    Add message model schema

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema ({
+
     senderId :  {
         type : mongoose.Schema.Types.ObjectId ,
         ref : "User" ,
         required : true
     } ,
+
     receiverId :  {
         type : mongoose.Schema.Types.ObjectId ,
         ref : "User" ,
         required : true
     },
+    
     message : {
         type : String ,
         required : true
