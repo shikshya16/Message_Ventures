c4c771957e935255dcebac1838e1a32aa8d2fcf2
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 20:45:57 +0545

    Add function to generate JWT token and set cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const generateTokenAndSetCookie = (userId , res) => {
-    const token = jwt.sign({userId} , process.env.JWT_SECRET ,{
+
+    const token = jwt.sign({userId} , process.env.JWT_SECRET ,
+      {
         expiresIn : '30d'
-     })
+     });
 
      res.cookie("jwt" , token , {
         maxAge: 15 * 24 * 60 * 60 * 1000 ,
