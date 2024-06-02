0a17793f882b714ca746caf904823c810920830b
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 20:31:36 +0545

    Add MongoDB connection setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
+
     try {
         await mongoose.connect(process.env.MONGODB_URI );
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log("Error connecting to MongoDB" , error.message);
     }
+    
 }
 
 module.exports = connectToMongoDB ;
