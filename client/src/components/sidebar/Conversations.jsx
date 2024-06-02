7c989784099a0025dd9e5474bd01d6db0da375dd
Shikshya Poudel <129182441+eeenola@users.noreply.github.com>
2024-06-02 21:06:01 +0545

    Conversations component to display list of chat conversations with loading indicator.

--- client/src/components/sidebar/Conversations.jsx
+++ client/src/components/sidebar/Conversations.jsx
@@ -4,7 +4,9 @@ import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
+
 	const { loading, conversations } = useGetConversations();
+	
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
 			{conversations.map((conversation, idx) => (
@@ -14,7 +16,9 @@ const Conversations = () => {
 					emoji={getRandomEmoji()}
 					lastIdx={idx === conversations.length - 1}
 				/>
-			))}
+			    )
+			  )
+			}
 
 			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
 		</div>
